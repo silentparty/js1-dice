e4cbe7bbfe0fe13a02b3dfd739ac36f59b9584be
Marcel Jacobs <silent.party@gmail.com>
2016-06-05 12:59:05 +1000

    got simple rolls working

--- dice/js/app.js
+++ dice/js/app.js
@@ -8,10 +8,55 @@ Creating a page where every time the user hits the "Roll Dice" button, the scree
 * generate a random number between 1 - 6 and store to a variable, random1
 * generate a random number between 1 - 6 and store to a variable, random2
 * combine 'dice-' and random1 to form the random class for the first die, firstDie
-* combine 'dice-' and random1 to form the random class for the second die, secondDie
+* combine 'dice-' and random2 to form the random class for the second die, secondDie
 * get the first die by ID and update the class to firstDie (hint: document.getElementById)
 * get the first die by ID and update the class to secondDie (hint:document.getElementById)
 
+
 3) Check to see if the Dice Roll has been hit, if it has run the diceRoll function.
 
+*/
+
+// function totesRandom() {
+//   return Math.floor(Math.random() * 6 + 1);
+// }
+//
+//
+// document.getElementById('roll-dice').onclick = function() {
+//   random1 = totesRandom();
+//   random2 = totesRandom();
+//   document.getElementById('first-die').className = "dice-" + random1;
+//   document.getElementById('second-die').className = "dice-" + random2;
+//   console.log(random1,random2);
+// }
+
+function totesRandom() {
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+document.getElementById('roll-dice').onclick = function() {
+  // var rolls = []
+  random1 = totesRandom();
+  random2 = totesRandom();
+  document.getElementById('first-die').className = "dice-" + random1;
+  document.getElementById('second-die').className = "dice-" + random2;
+  console.log(random1,random2);
+}
+
+/*
+As function declaration
+
+function totesRandom() {
+  return Math.floor(Math.random() * 6 + 1);
+}
+
+function diceRoll() {
+  random1 = totesRandom();
+  random2 = totesRandom();
+  document.getElementById('first-die').className = "dice-" + random1;
+  document.getElementById('second-die').className = "dice-" + random2;
+  console.log(random1,random2);
+}
+
+document.getElementById('roll-dice').onclick = function() { diceRoll() }
 */
